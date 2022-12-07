541b54fa3df139e73c06168514a15bbbfd93fa8f
LeonSKL <leokliaf@gmail.com>
2022-12-07 14:42:47 +0200

    BSAM with GNU AGPL

--- fuel.py
+++ fuel.py
@@ -1,5 +1,21 @@
-# -*- coding: utf-8 -*-
+"""
+    Copyright (C) 2022 Technoeconomics of Energy Systems laboratory - University of Piraeus Research Center (TEESlab-UPRC)
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+#
+# -*- coding: utf-8 -*-
 """
 This module is used for containing all fuel related data and doing the needed calculations
 """
--- greece_cost_benefits_calculator.py
+++ greece_cost_benefits_calculator.py
@@ -1,4 +1,21 @@
 """
+    Copyright (C) 2022 Technoeconomics of Energy Systems laboratory - University of Piraeus Research Center (TEESlab-UPRC)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+#
+"""
 This module is used to conduct cost-benefit calculations for the greek electricity market based on current subsidies framework & res projections
 """
 
